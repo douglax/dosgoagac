f82cb730513c926aceb8278f3d35bd283201ffa0
carlos.agu <carlos.agu@14dc7aca-6a66-11de-a2f9-0f955267958a>
2010-02-17 01:15:33 +0000

    Mas GUI

---
+++ ElectroFac/ElectroFacGUI/src/com/agac/gui/actions/NewEmisor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.agac.gui.actions;
+
+import com.agac.gui.EmisorTopComponent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public final class NewEmisor implements ActionListener {
+
+    public void actionPerformed(ActionEvent e) {
+        EmisorTopComponent top = new EmisorTopComponent();
+        top.open();
+        top.requestActive();
+    }
+}
