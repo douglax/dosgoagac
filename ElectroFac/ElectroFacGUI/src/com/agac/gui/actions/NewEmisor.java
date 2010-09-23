1cf92215ac576ca4bb07d0b988c0fd2922aec8d9
alxacostaa <alxacostaa@14dc7aca-6a66-11de-a2f9-0f955267958a>
2010-09-23 03:08:12 +0000

    Librerias de encriptación para el manejo de la licencia

--- ElectroFac/ElectroFacGUI/src/com/agac/gui/actions/NewEmisor.java
+++ ElectroFac/ElectroFacGUI/src/com/agac/gui/actions/NewEmisor.java
@@ -8,12 +8,52 @@ import com.agac.gui.EmisorTopComponent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
+//import com.agac.libraries.StringEncrypter;
+
 public final class NewEmisor implements ActionListener {
 
+
+
+    // Original
     @Override
     public void actionPerformed(ActionEvent e) {
         EmisorTopComponent top = new EmisorTopComponent();
         top.open();
         top.requestActive();
     }
+
+
+    // prueba
+
+//        @Override
+//    public void actionPerformed(ActionEvent e) {
+//
+//        System.out.println();
+//        System.out.println("+----------------------------------------+");
+//        System.out.println("|  -- Test Using Pass Phrase Method --   |");
+//        System.out.println("+----------------------------------------+");
+//        System.out.println();
+//
+//        String secretString = "Attack at dawn!";
+//        String passPhrase   = "My Pass Phrase";
+//
+//        // Create encrypter/decrypter class
+//        StringEncrypter desEncrypter = new StringEncrypter(passPhrase);
+//
+//        // Encrypt the string
+//        String desEncrypted       = desEncrypter.encrypt(secretString);
+//
+//        // Decrypt the string
+//        String desDecrypted       = desEncrypter.decrypt(desEncrypted);
+//
+//        // Print out values
+//        System.out.println("PBEWithMD5AndDES Encryption algorithm");
+//        System.out.println("    Original String  : " + secretString);
+//        System.out.println("    Encrypted String : " + desEncrypted);
+//        System.out.println("    Decrypted String : " + desDecrypted);
+//        System.out.println();
+//
+//    }
+
 }
