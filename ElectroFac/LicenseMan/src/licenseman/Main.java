110e913c977ab77dd5e639c7ec3db5b65fb9e585
alxacostaa <alxacostaa@14dc7aca-6a66-11de-a2f9-0f955267958a>
2010-09-27 21:45:30 +0000

    Un generador de licencias externo para el administrador (inconcluso)

---
+++ ElectroFac/LicenseMan/src/licenseman/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package licenseman;
+
+/**
+ *
+ * @author alejandro.acosta
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Licencias lic = new Licencias();
+        lic.setVisible(true);
+    }
+
+}
